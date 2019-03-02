266ab2030dd47cb91736d7d6ff4e964b9cc992ca
Fabien Leite <fabienleite@protonmail.com>
2019-03-02 21:59:12 +0100

    Correct code quality issues

--- src/search/SearchOnJulesJordan.ts
+++ src/search/SearchOnJulesJordan.ts
@@ -13,20 +13,22 @@ class SearchOnJulesJordan extends Search {
     const videoElements = $('.category_listing_wrapper_updates');
     for (let i = 0 ; i < videoElements.length ; i += 1) {
       const currentVideo: Video = new Video();
+      const currentVideoElement = videoElements.children().eq(i).children();
 
       // title
-      currentVideo.title = videoElements.children().eq(i).children().eq(2).text();
+      currentVideo.title = currentVideoElement.eq(2).text();
 
       // URLs
-      currentVideo.url = new URL(videoElements.children().eq(i).children().eq(2).attr('href'));
+      currentVideo.url = new URL(currentVideoElement.eq(2).attr('href'));
+      const thumbnails = currentVideoElement.eq(0).children().eq(0);
       currentVideo.thumbnailUrl = [];
       const nbrThmb = Number(
-        videoElements.children().eq(i).children().eq(0).children().eq(0).attr('cnt'),
+        thumbnails.attr('cnt'),
       );
 
       if (nbrThmb > 1) {
         for (let j = 0; j < nbrThmb ; j += 1) {
-          const currentThmb = videoElements.children().eq(i).children().eq(0).children().eq(0).attr(
+          const currentThmb = thumbnails.attr(
             (j === 1 ? 'src' : `src${j}_1x`),
           );
           if (typeof currentThmb !== 'undefined') {
@@ -36,31 +38,29 @@ class SearchOnJulesJordan extends Search {
         }
       } else {
         currentVideo.thumbnailUrl.push(
-          new URL(videoElements.children().eq(i).children().eq(0).children().eq(0).attr('src')),
+          new URL(thumbnails.attr('src')),
         );
       }
 
       // performers
       currentVideo.performers = [];
       // use of the function keyword for the anonymous function to keep context
-      videoElements.children().eq(i).children().eq(4).children().each(function () {
+      currentVideoElement.eq(4).children().each(function () {
         currentVideo.performers.push($(this).text());
       });
 
       // description
-      currentVideo.description = videoElements.children().eq(i).children().eq(2).attr('title');
+      currentVideo.description = currentVideoElement.eq(2).attr('title');
 
       // date
       currentVideo.date = new Date(
-        videoElements.children().eq(i).children().eq(6).children().eq(0).children().eq(0).text()
+        currentVideoElement.eq(6).children().eq(0).children().eq(0).text()
           .trim(),
       );
 
       // length
       let videoLength: number;
-      const videoLengthAndOtherInfos: string = (
-        videoElements.children().eq(i).children().eq(5).text()
-      );
+      const videoLengthAndOtherInfos: string = currentVideoElement.eq(5).text();
       const commaPosition: number = videoLengthAndOtherInfos.indexOf(',') + 1;
       const minWordPosition: number = videoLengthAndOtherInfos.indexOf('min');
       if (commaPosition && commaPosition > 0 && minWordPosition && minWordPosition > 0) {
