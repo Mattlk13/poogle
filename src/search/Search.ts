6ca15ad66cee79bff0e3dc6c35a8df54a2c50cb3
Fabien Leite <fabienleite@protonmail.com>
2019-03-04 20:20:12 +0100

    Refactor code for quality

--- src/search/Search.ts
+++ src/search/Search.ts
@@ -59,6 +59,49 @@ abstract class Search{
     );
   }
 
+  /**
+   * Finds and return the video title of a video element
+   * @param videoElement A Cheerio Element representing the pertinent node for the video
+   */
+  public abstract findVideoTitle(videoElement: Cheerio): string;
+
+  /**
+   * Finds and return the video URL of a video element
+   * @param videoElement A Cheerio Element representing the pertinent node for the video
+   */
+  public abstract findVideoURL(videoElement: Cheerio): URL;
+
+  /**
+   * Finds and return the URL of the thumbnails of a video element
+   * @param videoElement A Cheerio Element representing the pertinent node for the video
+   */
+  public abstract findVideoThumbnailsURL(videoElement: Cheerio): (URL)[];
+
+  /**
+   * Finds and return the name of the performers of a video element
+   * @param videoElement A Cheerio Element representing the pertinent node for the video
+   */
+  public abstract findVideoPerformers(videoElement: Cheerio): (string)[];
+
+  /**
+   * Finds and return the description of a video element
+   * @param videoElement A Cheerio Element representing the pertinent node for the video
+   */
+  public abstract findVideoDescription(videoElement: Cheerio): string;
+
+   /**
+   * Finds and return the date of a video element
+   * @param videoElement A Cheerio Element representing the pertinent node for the video
+   */
+  public abstract findVideoDate(videoElement: Cheerio): Date;
+
+  /**
+   * Finds and return the length of a video element in seconds
+   * @param videoElement A Cheerio Element representing the pertinent node for the video
+   */
+  public abstract findVideoLength(videoElement: Cheerio): number;
+
+  // should add findVideoTags(videoElement: Cheerio): (string)[]  when feature will be ready
 }
 
 export default Search;
--- src/search/SearchOnJulesJordan.ts
+++ src/search/SearchOnJulesJordan.ts
@@ -15,68 +15,83 @@ class SearchOnJulesJordan extends Search {
       const currentVideo: Video = new Video();
       const currentVideoElement = videoElements.children().eq(i).children();
 
-      // title
-      currentVideo.title = currentVideoElement.eq(2).text();
-
-      // URLs
-      currentVideo.url = new URL(currentVideoElement.eq(2).attr('href'));
-      const thumbnails = currentVideoElement.eq(0).children().eq(0);
-      currentVideo.thumbnailUrl = [];
-      const nbrThmb = Number(
-        thumbnails.attr('cnt'),
-      );
-
-      if (nbrThmb > 1) {
-        for (let j = 0; j < nbrThmb ; j += 1) {
-          const currentThmb = thumbnails.attr(
-            (j === 1 ? 'src' : `src${j}_1x`),
-          );
-          if (typeof currentThmb !== 'undefined') {
-            const currentThmbAsUrl = new URL(currentThmb);
-            currentVideo.thumbnailUrl.push(currentThmbAsUrl);
-          }
-        }
-      } else {
-        currentVideo.thumbnailUrl.push(
-          new URL(thumbnails.attr('src')),
-        );
-      }
-
-      // performers
-      currentVideo.performers = [];
-      // use of the function keyword for the anonymous function to keep context
-      currentVideoElement.eq(4).children().each(function () {
-        currentVideo.performers.push($(this).text());
-      });
-
-      // description
-      currentVideo.description = currentVideoElement.eq(2).attr('title');
-
-      // date
-      currentVideo.date = new Date(
-        currentVideoElement.eq(6).children().eq(0).children().eq(0).text()
-          .trim(),
-      );
-
-      // length
-      let videoLength: number;
-      const videoLengthAndOtherInfos: string = currentVideoElement.eq(5).text();
-      const commaPosition: number = videoLengthAndOtherInfos.indexOf(',') + 1;
-      const minWordPosition: number = videoLengthAndOtherInfos.indexOf('min');
-      if (commaPosition && commaPosition > 0 && minWordPosition && minWordPosition > 0) {
-        videoLength = Number(videoLengthAndOtherInfos.substr(
-            commaPosition,
-            minWordPosition - commaPosition,
-          ),
-        ); // get video length in minutes
-        videoLength *= 60 ; // convert in seconds
-      }
-      currentVideo.length = videoLength;
+      currentVideo.title = this.findVideoTitle(currentVideoElement);
+      currentVideo.url = this.findVideoURL(currentVideoElement);
+      currentVideo.thumbnailUrl = this.findVideoThumbnailsURL(currentVideoElement);
+      currentVideo.performers = this.findVideoPerformers(currentVideoElement);
+      currentVideo.description = this.findVideoDescription(currentVideoElement);
+      currentVideo.date = this.findVideoDate(currentVideoElement);
+      currentVideo.length = this.findVideoLength(currentVideoElement);
 
       videos.push(currentVideo);
     }
     return videos;
   }
+
+  public findVideoTitle(videoElement: Cheerio): string {
+    return videoElement.eq(2).text();
+  }
+
+  public findVideoURL(videoElement: Cheerio): URL {
+    return new URL(videoElement.eq(2).attr('href'));
+  }
+
+  public findVideoThumbnailsURL(videoElement: Cheerio): URL[] {
+    const thumbnails = videoElement.eq(0).children().eq(0);
+    const nbrThmb = Number(thumbnails.attr('cnt'));
+
+    if (nbrThmb > 1) {
+      const thumbnailUrl: (URL)[] = [];
+      for (let j = 0; j < nbrThmb ; j += 1) {
+        const currentThmb = thumbnails.attr(
+          (j === 1 ? 'src' : `src${j}_1x`),
+        );
+        thumbnailUrl.push(new URL(currentThmb));
+      }
+      return thumbnailUrl;
+    }
+    return [new URL(thumbnails.attr('src'))];
+
+  }
+
+  public findVideoPerformers(videoElement: Cheerio): string[] {
+    const performers: (string)[] = [];
+    // use of the function keyword for the anonymous function to keep context
+    const performersElement = videoElement.eq(4).children();
+    for (let i = 0 ; i < performersElement.length ; i += 1) {
+      performers.push(performersElement.eq(i).text());
+    }
+    return performers;
+  }
+
+  public findVideoDescription(videoElement: Cheerio): string {
+    return videoElement.eq(2).attr('title');
+  }
+
+  public findVideoDate(videoElement: Cheerio): Date {
+    return new Date(
+      videoElement.eq(6).children().eq(0).children().eq(0).text()
+        .trim(),
+    );
+
+  }
+
+  public findVideoLength(videoElement: Cheerio): number {
+    let videoLength: number;
+    const videoLengthAndOtherInfos: string = videoElement.eq(5).text();
+    const commaPosition: number = videoLengthAndOtherInfos.indexOf(',') + 1;
+    const minWordPosition: number = videoLengthAndOtherInfos.indexOf('min');
+    if (commaPosition && commaPosition > 0 && minWordPosition && minWordPosition > 0) {
+      videoLength = Number(videoLengthAndOtherInfos.substr(
+          commaPosition,
+          minWordPosition - commaPosition,
+        ),
+      ); // get video length in minutes
+      videoLength *= 60 ; // convert in seconds
+    }
+    return videoLength;
+  }
+
 }
 
 export default SearchOnJulesJordan;
